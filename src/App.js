83ad7635fd01d1795e9c7ac3b236d479b8951624
Nhan <natenguyen51294@gmail.com>
2020-07-27 20:17:30 -0500

    setting up layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import produce from 'immer'
+
+const numRows = 40
+const numCols = 40
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = []
+
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+    return rows
+  })
+
   return (
-    <div>
-      <h1>The Game of Life</h1>
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}>
+      {grid.map((rows, i) =>
+        rows.map((col, k) => <div
+          key={`${i}_${k}`}
+          onClick={() => {
+            const newGrid = produce(grid, copy => {
+              copy[i][k] = grid[i][k] ? 0 : 1
+            })
+            setGrid(newGrid)
+          }}
+          style={{
+            width: 20,
+            height: 20,
+            backgroundColor: grid[i][k] ? 'blue' : undefined,
+            border: 'solid 1px black'
+          }} />
+        ))}
     </div>
   )
 }
